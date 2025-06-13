e3c2db084e8d6c90c08967e81a2be3145e4ebe94
Jhojan Galvis <sayrakatalinagalvis@gmail.com>
2025-06-13 15:40:08 -0500

    Cambios Finales

--- Directorio-Personas/src/App.jsx
+++ Directorio-Personas/src/App.jsx
@@ -11,22 +11,68 @@ function App() {
           nombre="Juanito"
           cargo="Ingeniero"
           email="juanito@gmail.com"
-          imagen="Juanito.jfif"
+          imagen="Juanito.jpg"
           fechaNacimiento="2010-04-25"
         />
         <Directorio 
           nombre="Pepito"
           cargo="Maestro"
           email="pepito@gmail.com"
-          imagen="Pepito.jfif"
+          imagen="Pepito.jpg"
           fechaNacimiento="2004-02-13"
         />
         <Directorio 
           nombre="Pepita"
           cargo="Conductor"
           email="pepita@gmail.com"
-          imagen="Pepita.jfif"
+          imagen="Pepita.jpg"
           fechaNacimiento="1993-12-12"
+        />
+      </div>
+      <div className='Directorio'>
+        <Directorio 
+          nombre="Martin"
+          cargo="Ingeniero"
+          email="martin@gmail.com"
+          imagen="Hombre.jpg"
+          fechaNacimiento="2007-04-25"
+        />
+        <Directorio 
+          nombre="Martina"
+          cargo="Doctor"
+          email="martina@gmail.com"
+          imagen="Mujer.jpg"
+          fechaNacimiento="2003-02-13"
+        />
+        <Directorio 
+          nombre="Ermenegildo"
+          cargo="Inventor"
+          email="Ermenegildo@gmail.com"
+          imagen="Hombre2.jpg"
+          fechaNacimiento="1963-12-12"
+        />
+      </div>
+      <div className='Directorio'>
+        <Directorio 
+          nombre="Manuela"
+          cargo="Vendedora"
+          email="manuela@gmail.com"
+          imagen="Mujer2.jpg"
+          fechaNacimiento="1994-04-25"
+        />
+        <Directorio 
+          nombre="Manuel"
+          cargo="Contador"
+          email="manuel@gmail.com"
+          imagen="Hombre3.jpg"
+          fechaNacimiento="2004-02-13"
+        />
+        <Directorio 
+          nombre="Saray"
+          cargo="Bailarina"
+          email="saray@gmail.com"
+          imagen="Mujer3.jpg"
+          fechaNacimiento="2000-12-12"
         />
       </div>
     </div>
